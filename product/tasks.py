2af30ae328ce3bf11692b32fb1b642756ea49ab5
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-12-14 21:27:30 +0330

    Add some new task for getting the price of gold.

--- product/tasks.py
+++ product/tasks.py
@@ -1,28 +1,32 @@
-import time
-
 import requests
 
 from bs4 import BeautifulSoup
 from celery import shared_task
 from django.core.cache import cache
-from requests import Response
 
 
 @shared_task
 def get_gold_carat_24_price():
-    url = "https://www.tgju.org/profile/geram24"
+    url = "https://www.tgju.org/profile/geram24"  # Note: Illegal method
     result = requests.get(url)
     contents = BeautifulSoup(result.text, "html.parser")
     price = contents.find("span", {'data-col': 'info.last_trade.PDrCotVal'}).text
-    cache.set('gold_carat_24', price, timeout=1)
-    print(cache.get('gold_carat_24'))
+    return cache.set('gold_carat_24', price, timeout=10)
 
 
 @shared_task
 def get_gold_carat_18_price():
-    url = "https://www.tgju.org/profile/geram18"
+    url = "https://www.tgju.org/profile/geram18"  # Note: Illegal method
     result = requests.get(url)
     contents = BeautifulSoup(result.text, "html.parser")
     price = contents.find("span", {'data-col': 'info.last_trade.PDrCotVal'}).text
-    cache.set('gold_carat_18', price, timeout=1)
-    print(cache.get('gold_carat_18'))
+    return cache.set('gold_carat_18', price, timeout=10)
+
+
+@shared_task
+def get_gold_mesghal_price():
+    url = "https://www.tgju.org/profile/mesghal"  # Note: Illegal method
+    result = requests.get(url)
+    contents = BeautifulSoup(result.text, "html.parser")
+    price = contents.find("span", {'data-col': 'info.last_trade.PDrCotVal'}).text
+    return cache.set('gold_mesghal', price, timeout=10)
--- shop_api/celery.py
+++ shop_api/celery.py
@@ -1,13 +1,15 @@
 # celery -A shop_api beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler
+# celery -A shop_api worker -P solo -l INFO
+
 import os
 
 from celery import Celery
-from product.tasks import get_gold_carat_18_price, get_gold_carat_24_price
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop_api.settings.local')
 app = Celery('shop_api')
 
 app.conf.broker_url = f'redis://{os.getenv("HOSTNAME", "localhost")}:6379/0'
+app.conf.result_backend = 'redis://localhost:6379/0'
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 app.conf.beat_schedule = {
@@ -17,6 +19,9 @@ app.conf.beat_schedule = {
     },
     'get price gold carat 24': {
         'task': 'product.tasks.get_gold_carat_24_price',
+        'schedule': 1,
+    }, 'get gold mesghal': {
+        'task': 'product.tasks.get_gold_mesghal_price',
         'schedule': 1,
     },
 }
