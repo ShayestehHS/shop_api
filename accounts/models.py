ba325ef09bd9746c2881ccb1f7a012f81bc8e6d8
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-12-15 13:24:06 +0330

    Delete 'age' field from 'User' mdoel.

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    bio = models.CharField(max_length=200, help_text="Maximum length is 200.")
+    bio = models.CharField(max_length=200, null=True, blank=True, help_text="Maximum length is 200.")
     image = models.ImageField(default='image/avatar/default.jpg')
-    age = models.IntegerField()
