632e711d83b22a530626557dcc8146055f9ce65e
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-12-16 11:37:38 +0330

    Clear 'dj-rest-auth/' from path.

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -3,9 +3,8 @@ from django.urls import include, path
 from accounts.api import views
 
 urlpatterns = [
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('dj_rest-auth/registration/resend-email/', views.CustomResendEmailVerificationView.as_view(), name='rest_resend_email'),
-    path('dj-rest-auth/registration/account-confirm-email/<str:key>/', views.CustomConfirmEmailView.as_view(), name="account_confirm_email"),
-
+    path('', include('dj_rest_auth.urls')),
+    path('registration/', include('dj_rest_auth.registration.urls')),
+    path('registration/resend-email/', views.CustomResendEmailVerificationView.as_view(), name='rest_resend_email'),
+    path('registration/account-confirm-email/<str:key>/', views.CustomConfirmEmailView.as_view(), name="account_confirm_email"),
 ]
