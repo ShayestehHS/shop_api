68d10998d68b2202dfa0297c89132a6b748fd275
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-12-16 11:36:33 +0330

    Set minimum length for password.

--- shop_api/settings/base.py
+++ shop_api/settings/base.py
@@ -106,12 +106,16 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
+MINIMUM_LENGTH_PASSWORD = 9
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': MINIMUM_LENGTH_PASSWORD,
+        },
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
