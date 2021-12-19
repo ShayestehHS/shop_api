a4d3dbb9ca2958be9f94847a7bd29a9f8b45dea2
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-12-19 19:05:09 +0330

    Set '__str__' for 'cart' model.

--- cart/models.py
+++ cart/models.py
@@ -10,3 +10,6 @@ class Cart(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, blank=True)
     sum_prices = models.PositiveSmallIntegerField(default=0, help_text="Maximum valid integer is 2,147,483,647")
+
+    def __str__(self):
+        return f'cart of user:{self.user_id}'
