6a736f934ac9c99aee718fe36f9dea0a0d7b15fd
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-12-19 19:04:56 +0330

    Set '__str__' for 'user' model.

--- accounts/models.py
+++ accounts/models.py
@@ -5,3 +5,6 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     bio = models.CharField(max_length=200, null=True, blank=True, help_text="Maximum length is 200.")
     image = models.ImageField(default='image/avatar/default.jpg')
+
+    def __str__(self):
+        return self.username
