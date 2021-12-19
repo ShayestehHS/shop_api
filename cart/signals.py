a16896c0b215c030ab6d19c6b40c66e24e39b285
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-12-19 20:54:25 +0330

    Create signal for updating the sum_price field of 'cart'.

--- cart/signals.py
+++ cart/signals.py
@@ -2,8 +2,16 @@ from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
 
 from cart.models import Cart
+from product.models import Product
 
 
 @receiver(m2m_changed, sender=Cart.products.through)
-def toppings_changed(sender, instance, **kwargs):
-    print("changed")
+def products_changed(sender, instance: Cart, action, pk_set: set, **kwargs):
+    if 'post_' in action:  # post_remove, post_add, post_clear
+        products = Product.objects.filter(cart=instance).only('price')
+        sum_price = 0
+        for product in products:
+            sum_price += product.price
+
+        instance.sum_prices = sum_price
+        instance.save(update_fields=['sum_prices'])
