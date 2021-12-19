f658d52a8642aa6b8f5a3b8d86fbbaf0744aee7b
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-12-19 21:55:16 +0330

    Change ForeignKey to OneToOneField of 'Order' model.

--- order/models.py
+++ order/models.py
@@ -26,7 +26,7 @@ class Coupon(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    coupon = models.ForeignKey(Coupon, on_delete=models.CASCADE, null=True, blank=True)
+    coupon = models.OneToOneField(Coupon, on_delete=models.CASCADE, null=True, blank=True)
     payable_amount = models.DecimalField(max_digits=10, decimal_places=2)  # Read only
     address = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
