c83cebddf22a945e53ba83b1d296e1a41c1c7290
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-12-19 11:23:33 +0330

    Delete 'OrderItem' model.

--- order/admin.py
+++ order/admin.py
@@ -1,14 +1,6 @@
 from django.contrib import admin
 
-from order.models import Order, OrderItem
-
-
-@admin.register(OrderItem)
-class OrderItemModelAdmin(admin.ModelAdmin):
-    readonly_fields = ['calculated_discount', 'calculated_price', 'id']
-    list_per_page = 10
-    list_display_links = ['order']
-    list_display = ['order', 'product', 'calculated_price', 'calculated_discount']
+from order.models import Order
 
 
 @admin.register(Order)
--- order/models.py
+++ order/models.py
@@ -6,18 +6,9 @@ from product.models import Product
 User = settings.AUTH_USER_MODEL
 
 
-class OrderItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    order = models.ForeignKey('Order', on_delete=models.CASCADE)
-    tax = models.PositiveSmallIntegerField(default=settings.DEFAULT_TAX_PERCENT)
-    benefit = models.PositiveSmallIntegerField(default=settings.DEFAULT_BENEFIT_PERCENT)
-    wage = models.PositiveSmallIntegerField(help_text='Maximum valid wage per gram is 2,147,483,647')
-    calculated_price = models.DecimalField(max_digits=10, decimal_places=2)  # Read only
-    calculated_discount = models.DecimalField(max_digits=10, decimal_places=2)  # Read only
-
-
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     payable_amount = models.DecimalField(max_digits=10, decimal_places=2)  # Read only
     address = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
