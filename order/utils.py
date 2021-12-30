9a0ca82c26639772133d9a6f7c6e5c5c7e2b20c8
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-12-30 18:37:35 +0330

    Minor typo problem.

--- order/utils.py
+++ order/utils.py
@@ -6,6 +6,6 @@ def update_products_count(order: Order):
     products = Product.objects.filter(order=order)
     for product in products:
         product.count -= 1
-        if product.cont < 1: product.in_store = False
+        if product.count < 1: product.in_store = False
 
     Product.objects.bulk_update(products, ['count', 'in_store'])
