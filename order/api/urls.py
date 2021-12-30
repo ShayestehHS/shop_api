25ebd1b96324175344c6da11a325fd43481b370b
shayestehhs <hosseinshayesteh47.hs@gmail.com>
2021-12-30 15:32:14 +0330

    Write path for OrderDeleteAPIView.

--- order/api/urls.py
+++ order/api/urls.py
@@ -7,6 +7,7 @@ app_name = 'order'
 urlpatterns = [
     path('', views.OrderListAPIView.as_view(), name='list'),
     path('create/', views.OrderCreateAPIView.as_view(), name='create'),
+    path('delete/<int:id>/', views.OrderDeleteAPIView.as_view(), name='delete'),
     path('pay/', views.PaymentAPIView.as_view(), name='pay'),
     path('verify/', views.VerifyAPIView.as_view(), name='zp_verify'),
 ]
